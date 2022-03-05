550adf4e84790a6c9e42698f770040d2f13daf8d
JonathanDou <jonathandou100@gmail.com>
2022-03-05 14:36:28 -0500

    Commited

--- IHTB/Assets/Scripts/BulletSpawn.cs
+++ IHTB/Assets/Scripts/BulletSpawn.cs
@@ -2,19 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class BulletSpawn : MonoBehaviour
 {
+
+    [SerializeField] private GameObject _bulletPrefab; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnSimpleBullet", 2.0f, 0.3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
-    void SpawnBullet(transform )
+
+    void SpawnSimpleBullet()
+    {
+        Debug.Log("called!");
+        GameObject temp = Instantiate(_bulletPrefab, this.transform.position, Quaternion.identity) as GameObject;
+    }
+
+    /*void SpawnBullet(Vec2 location, Vec2 velocity)
+    {
+        GameObject temp = Instantiate(Resources.Load("DeathParticles"), location, Quaternion.identity) as GameObject;
+    }*/
+
 }
