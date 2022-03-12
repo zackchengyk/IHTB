a50fee544734b8c49328bb1d9ee765b55be37516
anhduong275 <haanhduong275@gmail.com>
2022-03-12 14:02:00 -0500

    unfixed ApproachUserBehavior func

---
+++ IHTB/Assets/Scripts/Seagulls/Behaviours/ApproachUserBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ApproachUserBehavior : SeagullBehaviour
+{
+    public Rigidbody2D player;
+
+   protected override void StartSeagullBehaviour()
+   {
+        // Set velocity
+        this._seagull.GetComponentInChildren<Rigidbody2D>().velocity = this.initialVelocity;
+
+        // Set transform rotation
+        SetSpriteRotationToVec2(this.initialVelocity.normalized);
+    }
+
+   protected override void UpdateSeagullBehaviour() { }
+}
+
